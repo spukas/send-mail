9cfbc5d1b82ce7bc4622439c592532194a0a4010
Linas <qlp@Linass-MacBook-Pro.local>
2017-11-18 16:44:48 +0200

    Add proxy

--- services/passport.js
+++ services/passport.js
@@ -19,6 +19,7 @@ passport.use(new GoogleStrategy({
   clientID: googleClientID,
   clientSecret: googleClientSecret,
   callbackURL: '/auth/google/callback',
+  proxy: true,
 }, (accessToken, refreshToken, profile, done) => {
   User.findOne({ googleId: profile.id })
     .then((existingUser) => {
