2e732686217c56b4d19120adefd4264e65493e88
Linas <qlp@Linass-MacBook-Pro.local>
2018-01-01 21:45:15 +0200

    Setup eslint with airbnb styleguide

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './index.css';
 
 const Landing = () => (
-  <div className={'landing-container'}>
+  <div className="landing-container">
     <h1>Emaily</h1>
-    <h4>Collect feedback from your users</h4>
+    <h4>Send emails and collect feedback from your users</h4>
   </div>
 );
 
