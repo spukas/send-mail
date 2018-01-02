eb01d625e6373e3a24db52135924156fc295f396
Linas Spukas <l.spukas@gmail.com>
2018-01-02 22:23:17 +0200

    Setup production keys on server side

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,2 @@
-// keys.js - fugure what credentials to return
-
-if (process.env.NODE_ENV === 'production') {
-  // return prod keys
-  module.exports = require('./prod');
-} else {
-  // return dev keys
-  module.exports = require('./dev');
-}
+module.exports =
+  process.env.NODE_ENV === 'production' ? require('./prod') : require('./dev');
