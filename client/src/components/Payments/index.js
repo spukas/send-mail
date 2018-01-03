be1a991fbf853c87a4897df1ce511c909527551c
Linas Spukas <l.spukas@gmail.com>
2018-01-03 20:03:29 +0200

    Add custom stripe button

--- client/src/components/Payments/index.js
+++ client/src/components/Payments/index.js
@@ -6,11 +6,15 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        name="Send Mail Eeasy"
+        description="Get 5 credits for 5$"
         amount={500}
         // eslint-disable-next-line
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
-      />
+      >
+        <button className="btn">Get credits</button>
+      </StripeCheckout>
     );
   }
 }
