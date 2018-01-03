9955ad8bf4b0066b6de7ffd2e03931c29c6f7c3b
Linas Spukas <l.spukas@gmail.com>
2018-01-03 20:16:13 +0200

    Add redux action creator to handle stripe token

--- client/src/components/Payments/index.js
+++ client/src/components/Payments/index.js
@@ -13,7 +13,7 @@ class Payments extends Component {
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
       >
-        <button className="btn">Get credits</button>
+        <button className="btn">Add credits</button>
       </StripeCheckout>
     );
   }
