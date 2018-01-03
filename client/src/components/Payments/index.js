fb66460bb620c08b79500a05252d6c53f37fa18e
Linas Spukas <l.spukas@gmail.com>
2018-01-03 20:29:39 +0200

    Connect redux action creator with StripeChekout component

--- client/src/components/Payments/index.js
+++ client/src/components/Payments/index.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 // eslint-disable-next-line
 class Payments extends Component {
+  static propTypes = {
+    handleToken: PropTypes.func.isRequired,
+  };
+
   render() {
     return (
       <StripeCheckout
@@ -10,7 +17,7 @@ class Payments extends Component {
         description="Get 5 credits for 5$"
         amount={500}
         // eslint-disable-next-line
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
       >
         <button className="btn">Add credits</button>
@@ -19,4 +26,4 @@ class Payments extends Component {
   }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
