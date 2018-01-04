05068e8d86673af153a3bbb681a6c9c7b0d2b1b9
Linas Spukas <l.spukas@gmail.com>
2018-01-04 08:34:56 +0200

    Add stripe billing route, create charge object, update user model with credits

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,5 +2,19 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = (app) => {
-  app.post('/api/stripe', (req, res) => {});
+  app.post('/api/stripe', async (req, res) => {
+    const { id: stripeToken, user: userModel } = req.body;
+    const chargeObject = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '5 credits for 5 $',
+      source: stripeToken,
+    });
+
+    // user model can by accessed through req.user, which is set up by passport
+    userModel.credits += 5;
+    const user = await userModel.save();
+
+    res.send(user);
+  });
 };
