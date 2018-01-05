6f66fb4f22cdec64e86d16887e97355094c0af1c
Linas Spukas <l.spukas@gmail.com>
2018-01-05 23:38:13 +0200

    Set up express to serve client side build assets if it does not recognize route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const billingRoutes = require('./routes/billingRoutes');
 const { mongoURI, cookieKey } = require('./config/keys');
@@ -23,6 +24,16 @@ app.use(passport.session());
 
 authRoutes(app);
 billingRoutes(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // serve production assets
+  app.use(express.static('client/build'));
+
+  // serve index.html file if express did not recognize route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'buid', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 // eslint-disable-next-line no-console
