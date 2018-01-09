8662e65d0ce69bcab30ac617686e1266b1deb774
Linas Spukas <l.spukas@gmail.com>
2018-01-09 22:41:49 +0200

    Add SendGrind lib for survey email management, set up api keys

--- config/prod.js
+++ config/prod.js
@@ -6,4 +6,5 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   stripePublicKey: process.env.STRIPE_PUBLIC_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY,
 };
