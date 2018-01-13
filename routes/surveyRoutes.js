8d1dc35655851c96d486fdd0ca7158a3de5d9874
Linas Spukas <l.spukas@gmail.com>
2018-01-13 18:28:15 +0200

    Add error handler to survey routes, make post request asynchronous, update survey and user model db

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,7 +9,7 @@ const Survey = mongoose.model('surveys');
 module.exports = (app) => {
   // TODO: use 'res'
   // eslint-disable-next-line no-unused-vars
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const {
       title, subject, body, recipients,
     } = req.body;
@@ -23,8 +23,18 @@ module.exports = (app) => {
       dateSent: Date.now(),
     });
 
-    // place to send mail
+    // send email, save survey, substract credits from user, update user
+    // return updated user object to the client
+    // if request will throw error, send response to the client with status and err object
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
