6115fec652cbd1e95f4389feeea5ff4e04e889cb
Linas Spukas <l.spukas@gmail.com>
2018-01-13 18:10:51 +0200

    Add more content to email template

--- services/mailTemplates/surveyTemplate.js
+++ services/mailTemplates/surveyTemplate.js
@@ -1,1 +1,17 @@
-module.exports = survey => `<div>${survey.body}</div>`;
+module.exports = survey => `
+  <html>
+    <body>
+      <div style="text-align: center">
+        <h3>We appreciate your opinion</h3>
+        <p>Please answer following question</p>
+        <p>${survey.body}</p>
+        <div>
+          <a href="http://localhost:3000">Yes</a>
+        </div>
+        <div>
+          <a href="http://localhost:3000">No</a>
+        </div>
+      </div>
+    </body>
+  </html>
+`;
